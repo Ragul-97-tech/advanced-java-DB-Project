8daa557bc7d9d64dd75a828d0d7b97dc5d3dc422
ragul.murugan <ragul.murugan@zohocorp.com>
2026-06-22 17:39:36 +0530

    Pythagoras Theorem Completion

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,11 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.*;
 
-class InvalidAgeException extends Exception {
-    public InvalidAgeException(String msg) {
-        super(msg);
-    }
-}
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-
-    public static void main(String[] args) throws InvalidAgeException {
+    public static void main(String[] args) {
         System.out.println(List.of("hi", "hello"));
+
+        System.out.println("Pythagoras Theorem: a² + b² = c²");
     }
-
-    /*public static void singleExecutor() {
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        Future<String> future = service.submit(() -> {
-            Scanner inputs = new Scanner(System.in);
-            return inputs.nextLine();
-        });
-
-        try {
-            String answer = future.get(10, TimeUnit.SECONDS);
-            System.out.println("Your answer: " + answer);
-        } catch (TimeoutException | InterruptedException | ExecutionException e) {
-            System.out.println("Time's up");
-            future.cancel(true);
-        }
-        service.shutdown();
-    }*/
 }
